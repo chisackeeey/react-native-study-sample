4ab553758338fc33a7d3a97ce89e61b5af26e95c
chisackeeey <chisaki_h@outlook.jp>
2020-06-18 00:06:11 +0900

    edit routing index

--- src/routes/Main/index.tsx
+++ src/routes/Main/index.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import { createStackNavigator, StackCardInterpolationProps } from '@react-navigation/stack';
 import * as UiContext from '../../contexts/ui';
-import { CHOOSE_LOGIN, HOME } from '../../constants/path';
+import { CHOOSE_LOGIN, HOME, LOADING, INITIAL } from '../../constants/path';
+import Loading from '../../components/pages/Loading/index';
+import ChooseLogin from '../../components/pages/ChooseLogin/index';
+import Home from '../../components/pages/Home/index';
+import Initial from '../../components/pages/Initial/index';
 
 const Stack = createStackNavigator();
 const forFade = ({ current }: StackCardInterpolationProps) => ({
@@ -13,18 +17,26 @@ const forFade = ({ current }: StackCardInterpolationProps) => ({
 function switchingAuthStatus(status: UiContext.Status) {
   switch (status) {
     case UiContext.Status.UN_AUTHORIZED:
-      return <Stack.Screen name={CHOOSE_LOGIN} component={CHOOSE_LOGIN} />;
+      return <Stack.Screen name={CHOOSE_LOGIN} component={ChooseLogin} />;
     case UiContext.Status.AUTHORIZED:
-      return <Stack.Screen name={HOME} component={HOME} />;
+      return <Stack.Screen name={HOME} component={Home} />;
     case UiContext.Status.FIRST_OPEN:
     default:
-      return <Stack.Screen name={HOME} component={HOME} />;
+      return <Stack.Screen name={INITIAL} component={Initial} />;
   }
 }
 
 function AuthWithRoutes() {
   const uiContext = React.useContext(UiContext.Context);
-  return <Stack.Navigator></Stack.Navigator>;
+  return (
+    <Stack.Navigator initialRouteName={LOADING} headerMode="none" screenOptions={{ cardStyleInterpoator: forFade }}>
+      {uiContext.applicationState !== UiContext.Status.LOADING ? (
+        switchingAuthStatus(uiContext.applicationState)
+      ) : (
+        <Stack.Screen name={LOADING} component={Loading} />
+      )}
+    </Stack.Navigator>
+  );
 }
 
 export default AuthWithRoutes;
