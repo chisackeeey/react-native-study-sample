6ae296be4d9c37d9d75dcf1f5906a0951d210ce5
chisackeeey <chisaki_h@outlook.jp>
2020-06-20 23:47:47 +0900

    edit App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
 
 import * as UiContext from './contexts/ui';
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-});
+import Routes from './routes';
 
 export default function App() {
   const [applicationState, setApplicationState] = React.useState(UiContext.createApplicationInitialState());
 
   return (
     <UiContext.Context.Provider value={{ applicationState, setApplicationState }}>
-      <View style={styles.container}>
-        <Text>Hello World</Text>
-      </View>
+      <Routes />
     </UiContext.Context.Provider>
   );
 }
