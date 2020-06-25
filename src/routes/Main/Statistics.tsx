08d12cec933ea2e541b070f7829ef3b48f327a71
chisackeeey <chisaki_h@outlook.jp>
2020-06-25 23:55:19 +0900

    apply HeaderLeft

--- src/routes/Main/Statistics.tsx
+++ src/routes/Main/Statistics.tsx
@@ -2,13 +2,20 @@ import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import { DETAIL, STATISTICS } from '../../constants/path';
 import { Detail, Statistics } from '../../components/pages';
+import { HeaderLeft } from '../Header';
 
 const Stack = createStackNavigator();
 
 export default function StatisticsNavigator() {
   return (
     <Stack.Navigator initialRouteName={STATISTICS}>
-      <Stack.Screen name={STATISTICS} component={Statistics} />
+      <Stack.Screen
+        name={STATISTICS}
+        component={Statistics}
+        options={{
+          headerLeft: () => <HeaderLeft />,
+        }}
+      />
       <Stack.Screen name={DETAIL} component={Detail} />
     </Stack.Navigator>
   );
--- src/routes/Main/UserInfo.tsx
+++ src/routes/Main/UserInfo.tsx
@@ -2,13 +2,20 @@ import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import { DETAIL, USER_INFO } from '../../constants/path';
 import { Detail, UserInfo } from '../../components/pages';
+import { HeaderLeft } from '../Header';
 
 const Stack = createStackNavigator();
 
 export default function UserInfoNavigator() {
   return (
     <Stack.Navigator initialRouteName={USER_INFO}>
-      <Stack.Screen name={USER_INFO} component={UserInfo} />
+      <Stack.Screen
+        name={USER_INFO}
+        component={UserInfo}
+        options={{
+          headerLeft: () => <HeaderLeft />,
+        }}
+      />
       <Stack.Screen name={DETAIL} component={Detail} />
     </Stack.Navigator>
   );
