f9006c51d5d9d9a7211d88beec73ff2f89b70f72
chisackeeey <chisaki_h@outlook.jp>
2020-06-29 23:45:53 +0900

    edit import path

--- src/components/pages/Statistics/index.tsx
+++ src/components/pages/Statistics/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { View, StyleSheet, Text, TouchableOpacity } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
-import { DETAIL } from 'src/constants/path';
+import { DETAIL } from '../../../constants/path';
 
 const styles = StyleSheet.create({
   container: {
