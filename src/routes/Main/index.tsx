bd0fc0570e01a124e612b0e99459cb5991a52455
chisackeeey <chisaki_h@outlook.jp>
2020-07-02 23:51:12 +0900

    edit routes Main

--- src/routes/Main/index.tsx
+++ src/routes/Main/index.tsx
@@ -3,8 +3,18 @@ import { createStackNavigator, StackCardInterpolationProps } from '@react-naviga
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 
-import { CHOOSE_LOGIN, HOME, LOADING, INITIAL, STATISTICS, USER_INFO, INPUT } from '../../constants/path';
-import { Initial, Loading, ChooseLogin, Input } from '../../components/pages';
+import {
+  CHOOSE_LOGIN,
+  HOME,
+  LOADING,
+  INITIAL,
+  STATISTICS,
+  USER_INFO,
+  INPUT,
+  SIGN_IN,
+  SIGN_UP,
+} from '../../constants/path';
+import { Initial, Loading, ChooseLogin, Input, SignIn, SignUp } from '../../components/pages';
 import Home from './Home';
 import Statistics from './Statistics';
 import UserInfo from './UserInfo';
@@ -13,6 +23,7 @@ import * as UiContext from '../../contexts/ui';
 
 const Stack = createStackNavigator();
 const ModalStack = createStackNavigator();
+const ChooseLoginStack = createStackNavigator();
 const Tab = createBottomTabNavigator();
 const HomeDrawer = createDrawerNavigator();
 const StatisticsDrawer = createDrawerNavigator();
@@ -78,10 +89,20 @@ function TabWithModalRoutes() {
   );
 }
 
+function ChooseLoginNavigator() {
+  return (
+    <ChooseLoginStack.Navigator initialRouteName={CHOOSE_LOGIN}>
+      <ChooseLoginStack.Screen name={CHOOSE_LOGIN} component={ChooseLogin} />
+      <ChooseLoginStack.Screen name={SIGN_IN} component={SignIn} />
+      <ChooseLoginStack.Screen name={SIGN_UP} component={SignUp} />
+    </ChooseLoginStack.Navigator>
+  );
+}
+
 function switchingAuthStatus(status: UiContext.Status) {
   switch (status) {
     case UiContext.Status.UN_AUTHORIZED:
-      return <Stack.Screen name={CHOOSE_LOGIN} component={ChooseLogin} />;
+      return <Stack.Screen name={CHOOSE_LOGIN} component={ChooseLoginNavigator} />;
     case UiContext.Status.AUTHORIZED:
       return <Stack.Screen name={HOME} component={TabWithModalRoutes} />;
     case UiContext.Status.FIRST_OPEN:
