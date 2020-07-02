bcaf4d0e7f88d26c907046b9248960a8f45408e8
chisackeeey <chisaki_h@outlook.jp>
2020-07-02 23:38:41 +0900

    add SignIn component

---
+++ src/components/pages/SignIn/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import { Context, Status } from '../../../contexts/ui';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+export default function Detail() {
+  const { setApplicationState } = React.useContext(Context);
+  return (
+    <View style={styles.container}>
+      <Text>Sign In</Text>
+      <TouchableOpacity onPress={() => setApplicationState(Status.AUTHORIZED)}>
+        <Text>Sign In</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
