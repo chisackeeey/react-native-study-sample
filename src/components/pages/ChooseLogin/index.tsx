b51dd6c69949149a9c4e3c4a9f1ba201ba12659f
chisackeeey <chisaki_h@outlook.jp>
2020-07-02 23:56:19 +0900

    edit ChooseLogin component

--- src/components/pages/ChooseLogin/index.tsx
+++ src/components/pages/ChooseLogin/index.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import { SIGN_IN, SIGN_UP } from '../../../constants/path';
 
 const styles = StyleSheet.create({
   container: {
@@ -10,9 +13,17 @@ const styles = StyleSheet.create({
 });
 
 export default function ChooseLogin() {
+  const { navigate } = useNavigation();
+
   return (
     <View style={styles.container}>
       <Text>ChooseLogin</Text>
+      <TouchableOpacity onPress={() => navigate(SIGN_IN)}>
+        <Text>go to Sign In</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => navigate(SIGN_UP)}>
+        <Text>go to Sign Up</Text>
+      </TouchableOpacity>
     </View>
   );
 }
