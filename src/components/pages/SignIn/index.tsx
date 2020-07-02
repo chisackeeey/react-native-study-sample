78b9352e7a7cf58e224a87561157b5f0687d0678
chisackeeey <chisaki_h@outlook.jp>
2020-07-02 23:58:35 +0900

    edit view

--- src/components/pages/SignIn/index.tsx
+++ src/components/pages/SignIn/index.tsx
@@ -15,7 +15,6 @@ export default function Detail() {
   const { setApplicationState } = React.useContext(Context);
   return (
     <View style={styles.container}>
-      <Text>Sign In</Text>
       <TouchableOpacity onPress={() => setApplicationState(Status.AUTHORIZED)}>
         <Text>Sign In</Text>
       </TouchableOpacity>
--- src/components/pages/SignUp/index.tsx
+++ src/components/pages/SignUp/index.tsx
@@ -15,7 +15,6 @@ export default function Detail() {
   const { setApplicationState } = React.useContext(Context);
   return (
     <View style={styles.container}>
-      <Text>Sign Up</Text>
       <TouchableOpacity onPress={() => setApplicationState(Status.AUTHORIZED)}>
         <Text>Sign Up</Text>
       </TouchableOpacity>
